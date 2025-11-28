43441098edc9525b39ff2312d6a3556159ca57c5
CoolGeorge <zack87608@gmail.com>
2025-11-27 23:02:03 -0700

    h

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -2,14 +2,14 @@ extends CharacterBody2D
 class_name Player
 
 @export var move_speed: float = 100
+@export var push_strength: float = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	position = SceneManager.player_spawn_position
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var move_vector: Vector2 = Input.get_vector("move_left","move_right","move_up","move_down")
 	
 	velocity = move_vector * move_speed
@@ -28,5 +28,20 @@ func _process(delta: float) -> void:
 	
 	else:
 		$AnimatedSprite2D.stop()
+	
+	
+	# Get the last collision
+	# Check if its the block
+	# If it is the block, push it
+	
+	var collision: KinematicCollision2D = get_last_slide_collision()
+	if collision:
+		
+		var collider_node = collision.get_collider()
+		
+		if collider_node.is_in_group("pushable"):
+			var collision_normal: Vector2 = collision.get_normal()
+			
+			collider_node.apply_central_force(-collision_normal * push_strength)
 	
 	move_and_slide()
