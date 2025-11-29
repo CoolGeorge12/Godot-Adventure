351d9ef91978248680065bd7b13f76cca970123c
CoolGeorge <zack87608@gmail.com>
2025-11-29 13:32:00 -0700

    new

--- Scenes/PuzzleButton/puzzle_button.gd
+++ Scenes/PuzzleButton/puzzle_button.gd
@@ -1,29 +1,26 @@
 extends Area2D
 
+@export var is_single_use: bool = false
+
 var bodies_on_top: int = 0
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+signal pressed
+signal unpressed
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("pushable") or body is Player:
 		bodies_on_top += 1
 		if bodies_on_top == 1:
-			print("I have been pushed!")
+			pressed.emit()
 			$AnimatedSprite2D.play("pressed")
 
 
 
 func _on_body_exited(body: Node2D) -> void:
+	if is_single_use:
+		return
+	
 	if body.is_in_group("pushable") or body is Player:
 		bodies_on_top -= 1
 		if bodies_on_top == 0:
-			print("I an no longer pushed!")
+			unpressed.emit()
 			$AnimatedSprite2D.play("unpressed")
